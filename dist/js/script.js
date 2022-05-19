e988f22ea87bd695f9ac58970046d15fe2900f16
Polina Ganina <poliganina@gmail.com>
2022-05-19 22:00:07 +0300

    adaptive about

--- dist/js/script.js
+++ dist/js/script.js
@@ -61,16 +61,3 @@ galleryItems.forEach(itemEl => {
     });
 });
 
-function onEntry(entry) {
-    entry.forEach(change => {
-      if (change.isIntersecting) {
-        change.target.classList.add('droits_show');
-      }
-    });
-  }
-  let options = { threshold: [0.5] };
-  let observer = new IntersectionObserver(onEntry, options);
-  let elements = document.querySelectorAll('.element-animation');
-  for (let elm of elements) {
-    observer.observe(elm);
-  }
